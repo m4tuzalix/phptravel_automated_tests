516d4280f23b34dc372264278adf7dee932c42fa
m4tuzalix <49403646+m4tuzalix@users.noreply.github.com>
2020-03-19 17:15:10 +0100

    Update JS_CASE2.py

--- JavaScript/JS_CASE2.py
+++ JavaScript/JS_CASE2.py
@@ -1,14 +1,13 @@
 js_flight_data = """
     function flight_data_gather(){
         const all_flights = document.querySelectorAll("form.row")
-        const random_flight = Math.floor(all_flights.length * Math.random())
-
-        const flight_details = all_flights[random_flight].querySelector("div[class='theme-search-results-item-flight-section']")
+       
+        const flight_details = all_flights[0].querySelector("div[class='theme-search-results-item-flight-section']")
         const flight_times = flight_details.querySelectorAll("p.theme-search-results-item-flight-section-meta-time")
         const flight_airline = flight_details.querySelector("h5.theme-search-results-item-flight-section-airline-title").innerText
         const flight_dates = flight_details.querySelectorAll("p.theme-search-results-item-flight-section-meta-date")
-        const flight_price = all_flights[random_flight].querySelector("input[name='flight_price']").getAttribute("value")
-        const enter_button = all_flights[random_flight].querySelector("button[class='btn btn-primary btn-block theme-search-results-item-price-btn']")
+        const flight_price = all_flights[0].querySelector("input[name='flight_price']").getAttribute("value")
+        const enter_button = all_flights[0].querySelector("button[class='btn btn-primary btn-block theme-search-results-item-price-btn']")
 
         const main_json = {"main":{
             "flight_times":Array.from(flight_times, time => time.innerText),
